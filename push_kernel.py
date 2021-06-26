488820de086eef869b47edecee72a941e5f1032a
Ottpocket <andrew.ott34@gmail.com>
2021-06-26 13:37:48 -0500

    Started copying run script

--- push_kernel.py
+++ push_kernel.py
@@ -1,9 +1,9 @@
-###################################################33
+###################################################
 #Pushes local python script to kaggle to run.
 #Specifically, this
 #1)  Creates EXPERIMENT_FOLDER in /Documents/python_scripts/create_trial/
-#2)  Places run_model.py in above folder
-#3)  Places json file in above folder
+#2)  Places json file in above folder
+#3)  Places run_model.py in above folder
 #4)  calls kaggle api to push script to kaggle
 
 
@@ -13,13 +13,19 @@ import os
 import sys
 sys.path.append('/home/aott/Documents/python_scripts/Helper_Functions/Helper_Functions')
 from __init__ import create_json
-parser = argparse.ArgumentParser()
 
+parser = argparse.ArgumentParser()
 parser.add_argument('--model_name')
 parser.add_argument('--cont_embeddings', default='MLP', type=str,choices = ['MLP','Noemb','pos_singleMLP'])
-
 args = parser.parse_args()
-print(args.model_name)
 
-#1) and 3)
-create_json(args.model_name)
+
+#1) and 2)
+DIRECTORY = '/home/aott/Documents/python_scripts/kaggle_stonk_directories'
+create_json(args.model_name, DIRECTORY=DIRECTORY)
+
+#3)
+from shutil import copyfile
+FILE = 'run_model.py'
+SOURCE = '/home/aott/Documents/python_scripts/create_trial'
+copyfile(os.path.join(SOURCE, FILE), os.path.join(DIRECTORY, FILE))
