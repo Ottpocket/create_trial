33378002d0ae30208eeb5fdd01216826c3fdb3fd
Ottpocket <andrew.ott34@gmail.com>
2021-06-28 06:04:28 -0500

    run_model.py now copied from kaggle kernel

--- run_model.py
+++ run_model.py
@@ -1,86 +1,126 @@
+#Base line predictions for a model.
 #feature engineering: https://alphascientist.com/feature_engineering.html
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 import os
-import pickle
-from tqdm.notebook import tqdm
-import matplotlib.pyplot as plt
 import gc
 from lightgbm import LGBMClassifier, LGBMRegressor
-import lightgbm
 from time import time
-df = pd.read_feather('/kaggle/input/stonk-size-explosion/features')
-from dateutil.relativedelta import relativedelta
-import datetime
+from sklearn.metrics import roc_auc_score
+from sklearn.linear_model import LogisticRegression
+from catboost import CatBoostClassifier
 
-from pip._internal import main as pipmain
-pipmain(['install', 'git+https://github.com/Ottpocket/Helper_Functions.git'])
-from Helper_Functions import reduce_mem_usage, get_val_test_increments
+!pip install git+https://github.com/Ottpocket/Helper_Functions.git
+from Helper_Functions import reduce_mem_usage, get_val_test_increments, get_preds
+import wandb
+
+HYPERPARAMS = dict( #Params
+                    model = 'lgbm', #lgbm, catboost, logistic
+                    tags = ['baseline', 'high_3'], #declares the features for the model. DELETE is just a filter for WandB
+                    TARGET = 'T_5day_2p',
+                    test_start = '2018-01-01',
+                    end_date = '2021-03-01',
+                    train_months = 3,
+                    intervals = 'year'
+                    )
+if HYPERPARAMS['model'] == 'lgbm': #tested and works
+    model = LGBMClassifier(objective='binary')
+elif HYPERPARAMS['model'] == 'catboost':
+    model = CatBoostClassifier()
+else:
+    model =LogisticRegression(n_jobs=-1) #tested and works
 
 df = pd.read_feather('/kaggle/input/create-stonk-data-from-daily-prices/stonk_df')
 
-base = ['day','open','high','low','close','adjclose','volume','ticker']
-targets = [target for target in df.columns if 'T_' in target]
-final = df[base + targets]
-final['preds'] = 0
+#Selecting the Features to be used
+FEATURES = []
+TARGET = HYPERPARAMS['TARGET']
 
-TEST_DATES = get_val_test_increments(end_date = end_date, test_start = test_start, intervals = intervals)
-start_day = TEST_DATES[0][0]
-df = df.loc[(df.day >=start_day), FEATURES].copy()
-################################################################################
-#Backtester: Trains model from set start date to set stop date.  Tests over next
-# specified time period.  Does this until specified end date.
-#INPUTS:
-    #experiment_name: (str) name of experiment
-    #data_start_date: ('yyyy-mm-dd') date beginning train data
-    #test_start_date: ('yyyy-mm-dd') date beginning test data
-    #end_date: ('yyyy-mm-dd') final date in test data
-    #intervals: ('2week','month','year') how often to test data
-    #model: model that has a fit and predict method
-    #val_period: (int) number of days prior to test with which to validate
+if 'baseline' in HYPERPARAMS['tags']:
+    FEATURES += ['open', 'high', 'low', 'close', 'volume']
 
-def get_preds(stonks, experiment_name, data_start_date, test_start, end_date, intervals, model,
-             FEATURES, TARGET, val_period = None):
-    stonks[experiment_name] = -99
-    start_msk = stonks['day'] > data_start_date
+if 'high_3' in HYPERPARAMS['tags']:
+    FEATURES+= [ 'r_high_mean_3', 'r_high_var_3',
+       'r_high_min_3', 'r_high_max_3']
 
-    #Gives list of tuples.  Each tuple is start and end dates for test
-    TEST_DATES = get_val_test_increments(end_date = end_date, test_start = test_start, intervals = intervals)
-    TEST_DATES.reverse()
-    for i, (train_start, test_start, test_finish) in tqdm(enumerate(TEST_DATES)):
-        print(f'iteration {i} of {len(TEST_DATES)}')
-        #Creating temporary train
-        msk = (stonks.day < test_start) & (stonks['day'] > train_start)
-        train = np.zeros( (np.sum(msk), len(FEATURES)), dtype=np.float32)
-        for col_idx, feat in enumerate(FEATURES):
-            train[:,col_idx] = stonks.loc[msk, feat].values.astype(np.float32)
+if 'high_5' in HYPERPARAMS['tags']:
+    FEATURES+= [ 'r_high_mean_5', 'r_high_var_5',
+       'r_high_min_5', 'r_high_max_5']
 
-        train_TARGET = stonks.loc[msk, TARGET].values
-        del msk; gc.collect()
-        print('finished train')
-
-        #Creating temporary test
-        test_msk = (stonks.day>=test_start) & (stonks.day <= test_finish) & (~stonks[TARGET].isnull())
-        test = np.zeros( (np.sum(test_msk), len(FEATURES)), dtype=np.float32)
-        for col_idx, feat in enumerate(FEATURES):
-            test[:,col_idx] = stonks.loc[test_msk, feat].values.astype(np.float32)
-        print('Finished test')
+FEATURES = list(set(FEATURES))
 
 
-        #Getting preds if test exists
-        if test.shape[0] >0:
-            #Training model
-            model.fit(train, train_TARGET)
-            stonks.loc[test_msk, experiment_name] = model.predict(test)
-            print(f'Model Finished')
 
-        print(f'Test Start: {test_start}, Test Finish: {test_finish} ,Train: {train.shape}, Test: {test.shape}')
-        print()
-        del train, test, test_msk; gc.collect()
+df['T_10day_10p'] = df['T_high_max_10'] >= 1.1 * df['close']
+df['T_10day_5p'] = df['T_high_max_10'] >= 1.05 * df['close']
+df['T_10day_2p'] = df['T_high_max_10'] >= 1.02 * df['close']
 
-model = LGBMRegressor()
-TARGET = '2_week_high'
-FEATURES = list(df.columns.drop([TARGET, 'day', 'ticker']))
+df['T_5day_10p'] = df['T_high_max_5'] >= 1.1 * df['close']
+df['T_5day_5p'] = df['T_high_max_5'] >= 1.05 * df['close']
+df['T_5day_2p'] = df['T_high_max_5'] >= 1.02 * df['close']
+df['T_10day_closeratio'] = df.groupby('ticker', sort=False)['close'].shift(-10).reset_index(drop=True) / df['close']
+df['T_5day_closeratio'] = df.groupby('ticker', sort=False)['close'].shift(-5).reset_index(drop=True) / df['close']
+
+#Initializing WandB
+!wandb login 139a106845d441074259f3d8b48ab85719b377cf
+
+#See https://docs.wandb.com/library/init for more details
+run = wandb.init(project='Week_2p', config=HYPERPARAMS, tags = HYPERPARAMS['tags'])
+config = wandb.config
+
+#model
 start_ = time()
-get_preds(df, 'cow_exp', '2000-01-01', '2007-01-01','2021-03-01', 'month', model, FEATURES, TARGET)
+get_preds(stonks = df, test_start = HYPERPARAMS['test_start'],
+          end_date = HYPERPARAMS['end_date'],
+          train_months = HYPERPARAMS['train_months'],
+          intervals = HYPERPARAMS['intervals'],
+          model = model,
+          FEATURES = FEATURES,
+          TARGET = HYPERPARAMS['TARGET'])
 print(f'Took {time() - start_ :.2f} seconds')
+
+#Saving the predictions
+df = df[df.day >= HYPERPARAMS['test_start']].reset_index(drop=True).copy()
+df.to_feather('stonk_df')
+df.sort_values(['day','preds'], ascending=[True, False], inplace = True)
+#Expected return on a preds >=.8
+df['return'] = np.nan
+df.loc[df[TARGET], 'return'] = 2
+df.loc[~df[TARGET], 'return'] = df.loc[~df[TARGET], 'T_10day_closeratio']
+Expected_Return_5 = df.loc[(df['preds'] >= .8), 'return'].mean()
+print(f'Expected Return with selling bad at 5 day close: {Expected_Return_5}')
+wandb.run.summary["Expected_Return_5"] = Expected_Return_5
+
+#days w/ >= 1 prediction greater than .8
+perc = .8
+df['ge_perc'] = df['preds'] >= perc
+agg = df.groupby('day').agg({'ge_perc':'sum'})
+Perc_Predict_Days = np.mean(agg['ge_perc'] > 0)
+print(f'A prediction of .8 confidence made in {Perc_Predict_Days :.3f}% of days')
+wandb.run.summary["Perc_Predict_Days"] = Perc_Predict_Days
+
+#Correlation between prediction and TARGET
+Correlation = df[[TARGET, 'preds']].corr().values[0,1]
+print(f'Correlation: {Correlation :.3f}')
+wandb.run.summary["Correlation"] = Correlation
+
+# Accuracy of predictions at >=.8 confidence
+Acc = df.loc[df.preds >=.8, [TARGET]].mean().values[0]
+print(f'Accuracy of preds >=80%: {Acc :.3f}')
+wandb.run.summary["Acc"] = Acc
+
+#AUC
+AUC = roc_auc_score(df[TARGET], df['preds'])
+print(f'AUC: {AUC :.3f}')
+wandb.run.summary["AUC"] = AUC
+
+#ACC pf top 10 preds each day >=.8
+df2 = df.groupby('day').head(10)
+Top_10_ACC = df2.loc[df2.preds >=.8, TARGET].mean()
+print(f'Top 10 Acc: {Top_10_ACC :.3f}')
+wandb.run.summary["Top_10_ACC"] = Top_10_ACC
+
+#Return of top 10 preds each day >=.8
+Top_10_Return= df2.loc[df2.preds >=.8, 'return'].mean()
+print(f'Top 10 Return: {Top_10_Return :.3f}')
+wandb.run.summary["Top_10_Return"] = Top_10_Return
