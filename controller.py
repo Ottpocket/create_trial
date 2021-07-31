75d78ecadbf17dd1ad881d73b85a4c674cab96fa
Ottpocket <andrew.ott34@gmail.com>
2021-07-31 14:41:07 -0500

    Updated controller.py

--- controller.py
+++ controller.py
@@ -19,24 +19,34 @@ all_train_months = [1,2,3,6,12]
 all_targets = ['T_high_max_5', 'T_openClose_lag_-1']
 PARAMETERS_FOR_MODELS = {} #Dict with hash as key and parameters as value
 for n_days in range(1, len(all_days)+1):
+    days = all_days[:n_days]
     for months in all_train_months:
         for target in all_targets:
             model_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-            PARAMETERS_FOR_MODELS[model_name] = {'FEATURES': ,
-                                                 'TARGET': ,
-                                                  'TRAIN_MONTHS': }
+            PARAMETERS_FOR_MODELS[model_name] = {'FEATURES': days,
+                                                 'TARGET': months,
+                                                  'TRAIN_MONTHS': target}
 
 
 #put models in queue and run n simultanious models on kaggle
 MAX_KAGGLE_KERNELS_RUNNING = 5
 active_models_on_kaggle = {}#Model:status either model or backtest
-for key in PARAMETERS_FOR_MODELS.keys():
+for idx, key in enumerate(PARAMETERS_FOR_MODELS.keys()):
+
+    FEATURES = PARAMETERS_FOR_MODELS[key][FEATURES]
+    TARGET = PARAMETERS_FOR_MODELS[key][TARGET]
+    TRAIN_MONTHS = PARAMETERS_FOR_MODELS[key][TRAIN_MONTHS]
+
     #kickoff model in kaggle
     kick_off_model(FEATURES, TARGET, TRAIN_MONTHS)
     active_programs_on_kaggle[key] = 'model'
 
-    #Wait until you can run more programs
-    while len(active_models_on_kaggle.keys()) > MAX_KAGGLE_KERNELS_RUNNING:
+
+    if idx == (len(PARAMETERS_FOR_MODELS.keys()) - 1):
+        condition = len(active_models_on_kaggle.keys()) > MAX_KAGGLE_KERNELS_RUNNING
+    else:
+        condition = len(active_models_on_kaggle.keys()) ==0
+    while condition:
         for running_key in active_models_on_kaggle.keys():
 
 
@@ -52,6 +62,17 @@ for key in PARAMETERS_FOR_MODELS.keys():
             elif active_models_on_kaggle[running_key] == 'backtest':
                 status = subprocess.run(f'kaggle kernels status Ottpocket/{running_key}'.split(' '), stdout=subprocess.PIPE)
                  if 'complete' in status:
-                     del active_models_on_kaggle[running_key]
+                     active_models_on_kaggle[running_key] = 'complete'
 
             #elif check errored status
+
+        #Delete all completed and errored out
+        for key in active_models_on_kaggle.keys():
+            if active_models_on_kaggle[key].isin(['complete','error']):
+                del active_models_on_kaggle[key]
+
+        
+        if idx == (len(PARAMETERS_FOR_MODELS.keys()) - 1):
+            condition = len(active_models_on_kaggle.keys()) > MAX_KAGGLE_KERNELS_RUNNING
+        else:
+            condition = len(active_models_on_kaggle.keys()) ==0
